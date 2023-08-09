c2e8d669c6ac75210267a481817abdfccd181df5
Dylan Bourque <52940997+Bourqu@users.noreply.github.com>
2023-08-09 19:42:56 -0400

    appending numbers and clear has been added

---
+++ src.js
@@ -0,0 +1,52 @@
+class Calculator {
+  constructor(screenText) {
+    this.screenText = screenText;
+    this.clear();
+  }
+
+  clear() {
+    this.screen = "";
+  }
+
+  delete() {}
+
+  appendNumber(number) {
+    this.screen = number;
+  }
+
+  updateDisplay() {
+    this.screenText.innerText = this.screen;
+  }
+}
+
+const screen = document.querySelector(".calc-screen");
+
+//all numbers
+const numberButtons = document.querySelectorAll("[data-number]");
+
+//functions
+const equalButton = document.querySelector("[data-equal]");
+const deleteButton = document.querySelector("[data-delete]");
+const resetButton = document.querySelector("[data-reset");
+const decimalButton = document.querySelector("[data-decimal]");
+
+//operations
+const plusButton = document.querySelector("[data-plus");
+const minusButton = document.querySelector("[data-minus]");
+const multiplyButton = document.querySelector("[data-multiply]");
+const divideButton = document.querySelector("[data-divide]");
+
+const calculator = new Calculator(screen);
+
+//add an event listener
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.appendNumber(button.innerText);
+    calculator.updateDisplay();
+  });
+});
+
+resetButton.addEventListener("click", () => {
+  calculator.clear();
+  calculator.updateDisplay();
+});
